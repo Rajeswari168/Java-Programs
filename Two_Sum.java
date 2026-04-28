5234e547e7f944339d6c6aa484a6580990d1283a
Rajeswari168 <mrajeswariraji66@gmail.com>
2026-04-28 17:51:43 +0530

    Create Two_Sum.java

---
+++ Two_Sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();        
+        int nums[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(nums[i] + nums[j] == target){
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
+        }
+    }
+}
