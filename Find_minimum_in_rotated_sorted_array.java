3e075607535d33d5ffc599a0a081a2af4c0e6945
Rajeswari168 <mrajeswariraji66@gmail.com>
2026-05-15 18:44:40 +0530

    Implement minimum finder in rotated sorted array

---
+++ Find_minimum_in_rotated_sorted_array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int min = nums[0];
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
