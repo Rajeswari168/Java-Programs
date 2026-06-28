5bdfd4d307e567373b1f79a0e39aac5339daa888
Rajeswari168 <mrajeswariraji66@gmail.com>
2026-06-28 10:23:27 +0530

    Add solution for maximum element after rearranging
    Implement logic to find maximum element after decreasing and rearranging.

---
+++ Maximum_element_after_decreasing_and_rearranging.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        arr[0]=1;
+        for(int i=1;i<n;i++){
+            if(arr[i] > arr[i-1]+1){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        System.out.print(arr[n-1]);
+    }
+}
