7964a2f85d1424e3471d4acb0cd32b79ba64ec61
Rajeswari168 <mrajeswariraji66@gmail.com>
2026-07-01 09:52:23 +0530

    Implement Contains Duplicates II solution

---
+++ Contains_duplicates_II.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j] && (j - i) <= k) {
+                    System.out.print(true);
+                    return;
+                }
+            }
+        }
+        System.out.print(false);
+    }
+}
